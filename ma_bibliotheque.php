c0338ae5b03527d312237790e054ccdfdb318f0f
WaelRayane <98024026+WaelRayane@users.noreply.github.com>
2022-03-15 16:10:03 +0100

    Update ma_bibliotheque.php

--- ma_bibliotheque.php
+++ ma_bibliotheque.php
@@ -13,9 +13,18 @@ function trouver_min($tableau) {
     return $min;
 }
 
-// Fonction pour trouver la valeur maximale d'un tableau
+
 function trouver_max($tableau) {
-    // TODO: Écrire le corps de la fonction
+   $max = $tableau[0];
+
+    foreach ($tableau as $element) {
+        if ($max < $element) {
+            $max = $element;
+        }
+    }
+
+    return $max;
+      
 }
 
 // Fonction pour trouver la valeur la plus proche de zéro
